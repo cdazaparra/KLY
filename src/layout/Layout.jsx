ab70d04ae82892dbc4ae25f3a2396d5f8d811877
Andres Daza <cdazaparra@gmail.com>
2023-04-19 16:43:04 -0500

    gh-pages

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = () => {
       <Menu></Menu>
       <main className="m-0 justify-items-center bg-gray-100 p-0 items-center">
         {navigation.state === "loading" && (
-          <div className="alert alert-info my-5">Loading...</div>
+          <div className="alert alert-info m-5 text-xl">Loading...</div>
         )}
         <Outlet></Outlet>
       </main>
